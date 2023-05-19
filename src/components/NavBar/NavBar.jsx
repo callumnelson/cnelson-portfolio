93b30e946d873881975eab4647aa75526a01cb24
Callum Nelson <callum.nelson95@gmail.com>
2023-05-18 20:22:11 -0400

    refactor navbar and bring links inside of nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,6 @@
+// modules
+import { Link } from 'react-router-dom'
+
 // Components & Assets
 import Headshot from '../../assets/headshot.png'
 import Clock from '../Clock/Clock'
@@ -9,12 +12,28 @@ const NavBar = ({setDaytime}) => {
   
   return (
     <nav className={styles.navbar}>
-      <img src={ Headshot } alt="Callum Nelson Headshot" />
-      <h1>Callum Nelson</h1>
-      <p>About</p>
-      <p>Resume</p>
-      <p>Portfolio</p>
-      <Clock setDaytime={setDaytime}/>
+      <div className={styles.name}>
+        <img src={ Headshot } alt="Callum Nelson Headshot" />
+        <h1>Callum</h1>
+        <h1>Nelson</h1>
+      </div>
+      <div className={styles.destinations}>
+        <p>About</p>
+        <p>Resume</p>
+        <p>Portfolio</p>
+        <Clock setDaytime={setDaytime}/>
+      </div>
+      <div className={styles.footer}>
+        <Link to="https://github.com/callumnelson" target="_blank" rel="noreferrer">
+          <i className="fa-brands fa-github fa-xl"></i>
+        </Link>
+        <Link to="mailto:callum.nelson95@gmail.com" target="_blank" rel="noreferrer">
+          <i className="fa-solid fa-envelope fa-xl"></i>
+        </Link>
+        <Link to="https://www.linkedin.com/callum-nelson" target="_blank" rel="noreferrer">
+          <i className="fa-brands fa-linkedin fa-xl"></i>
+        </Link>
+      </div>
     </nav>
   )
 }
