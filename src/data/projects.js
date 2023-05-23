1be13d0afbd4ddf84f04f802e5910d535e50286f
Callum Nelson <callum.nelson95@gmail.com>
2023-05-23 11:41:06 -0400

    crop ga icon, refactor experience card, and update experiences and projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'appliCANt',
     screenshot: 'applicant',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestiae nisi itaque labore repellendus perferendis, totam ea, animi aut amet distinctio sequi, quisquam officia! Dolor tempore optio veniam quaerat quisquam?',
+    description: 'appliCANt is a one-stop-shop for managing your job search. The platform incorporates resources to support the job search (e.g. interview prep guides) with a seamless, spreadsheet-style interface for tracking your applications.',
     technologies: ['REACT', 'MUI', 'MONGODB', 'EXPRESS', 'NODE'],
     githubLink: 'https://github.com/callumnelson/applicant-front-end',
     deployedLink: 'https://job-applicant.netlify.app/'
@@ -10,7 +10,7 @@ export const projects = [
   {
     title: 'Battle of the Books',
     screenshot: 'battleOfTheBooks',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestiae nisi itaque labore repellendus perferendis, totam ea, animi aut amet distinctio sequi, quisquam officia! Dolor tempore optio veniam quaerat quisquam?',
+    description: `Battle of the Books takes teachers' classroom libraries digital. After being admitted to the site by their teacher, students can track their reading and submit tickets to receive credit for finishing books. Battle of the Books consumes Google's Books API to seamlessly pull accurate information on titles, authors, and page numbers.`,
     technologies: ['MONGODB', 'EXPRESS', 'NODE', 'EJS'],
     githubLink: 'https://github.com/callumnelson/battle-of-the-books',
     deployedLink: 'https://battle-of-the-books.fly.dev/'
@@ -18,7 +18,7 @@ export const projects = [
   {
     title: 'Shakespearean Othello',
     screenshot: 'othello',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestiae nisi itaque labore repellendus perferendis, totam ea, animi aut amet distinctio sequi, quisquam officia! Dolor tempore optio veniam quaerat quisquam?',
+    description: 'Shakespearean Othello is an Elizabethan-era spin on the classic board game, Othello. Users can play one another or try their luck against three levels of computer player, which utilizes a recursive MiniMax algorithm to compute its next best move.',
     technologies: ['JAVASCRIPT', 'HTML', 'CSS', 'GIT'],
     githubLink: 'https://github.com/callumnelson/othello',
     deployedLink: 'https://shakespearean-othello.netlify.app/'
