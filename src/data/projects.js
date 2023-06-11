03dff273b457fb94ab59e880efb2720f174881a8
Callum Nelson <callum.nelson95@gmail.com>
2023-06-11 09:16:46 -0400

    reorder portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,18 +8,10 @@ export const projects = [
     deployedLink: 'https://savvier.netlify.app',
   },
   {
-    title: 'callumnelson.dev',
-    screenshot: 'portfolio',
-    description: `I built this site using React with Vite and a serverless Google Cloud Function-based back-end as a way to share my portfolio and experiences with the world. I'll continue to update it with new work as I finish projects, so come back soon for more updates!`,
-    technologies: ['REACT', 'VITE', 'GCF', 'CSS'],
-    githubLink: 'https://github.com/callumnelson/cnelson-portfolio',
-    deployedLink: 'https://callumnelson.dev',
-  },
-  {
     title: 'appliCANt',
     screenshot: 'applicant',
     description:
-      'appliCANt is a one-stop-shop for managing your job search. The platform is a MERN stack app that incorporates resources to support the job search (e.g. interview prep guides) with a seamless, spreadsheet-style interface for tracking your applications.',
+    'appliCANt is a one-stop-shop for managing your job search. The platform is a MERN stack app that incorporates resources to support the job search (e.g. interview prep guides) with a seamless, spreadsheet-style interface for tracking your applications.',
     technologies: ['REACT', 'MUI', 'MONGODB', 'EXPRESS', 'NODE'],
     githubLink: 'https://github.com/callumnelson/applicant-front-end',
     deployedLink: 'https://job-applicant.netlify.app/',
@@ -36,9 +28,17 @@ export const projects = [
     title: 'Shakespearean Othello',
     screenshot: 'othello',
     description:
-      'Shakespearean Othello is an Elizabethan-era spin on the classic board game, Othello. Users can play one another or try their luck against three levels of computer player, which utilizes a recursive MiniMax algorithm to compute its next best move.',
+    'Shakespearean Othello is an Elizabethan-era spin on the classic board game, Othello. Users can play one another or try their luck against three levels of computer player, which utilizes a recursive MiniMax algorithm to compute its next best move.',
     technologies: ['JAVASCRIPT', 'HTML', 'CSS', 'GIT'],
     githubLink: 'https://github.com/callumnelson/othello',
     deployedLink: 'https://shakespearean-othello.netlify.app/',
+  },
+  {
+    title: 'callumnelson.dev',
+    screenshot: 'portfolio',
+    description: `I built this site using React with Vite and a serverless Google Cloud Function-based back-end as a way to share my portfolio and experiences with the world. I'll continue to update it with new work as I finish projects, so come back soon for more updates!`,
+    technologies: ['REACT', 'VITE', 'GCF', 'CSS'],
+    githubLink: 'https://github.com/callumnelson/cnelson-portfolio',
+    deployedLink: 'https://callumnelson.dev',
   },
 ]
